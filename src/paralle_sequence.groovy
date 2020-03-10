2a887c9d6d5aca4e419af4d3d8711bc26e620a40
cdh <dh.chandu@gmail.com>
2020-03-10 17:03:52 +0530

    parallel sequence

---
+++ src/paralle_sequence.groovy
@@ -0,0 +1,18 @@
+def stepsToRun = [:]
+
+pipeline {
+    agent none
+
+    stages {
+        stage ("Prepare Stages"){
+            steps {
+                script {
+                    for (int i = 1; i < 5; i++) {
+                        stepsToRun["Step${i}"] = prepareStage "Step${i}"
+                    }
+                    parallel stepsToRun
+                }
+            }
+        }
+    }
+}
