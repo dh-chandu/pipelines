5626768bf54bf6d430d862486428e9783951b625
cdh <dh.chandu@gmail.com>
2020-03-11 16:40:57 +0530

    dynamic

--- vars/ui.groovy
+++ vars/ui.groovy
@@ -1,16 +1,22 @@
 def call(String name = 'human') {
      return {
-        stage (name) {
-            stage(name+"checkout") {
-                echo "start 1"
-                sleep 1
-                echo "done 1"
-            }
-            stage("build") {
-                echo "start 2"
-                sleep 1
-                echo "done 2"
-            }
-        }
+         pipeline {
+             agent any
+             stages {
+                 stage (name) {
+                     stage(name+"checkout") {
+                         echo "start 1"
+                         sleep 1
+                         echo "done 1"
+                     }
+                     stage("build") {
+                         echo "start 2"
+                         sleep 1
+                         echo "done 2"
+                     }
+                 }
+             }
+         }
+
     }
 }
