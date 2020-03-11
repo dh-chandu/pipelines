f3e1e3473de3807a0341937d10fd3fc4b71fa9c9
cdh <dh.chandu@gmail.com>
2020-03-11 12:08:49 +0530

    dynamic function call using shared libs class

--- src/jnpr/releng/Tester.groovy
+++ src/jnpr/releng/Tester.groovy
@@ -2,17 +2,17 @@
 package jnpr.releng
 
 public class Tester implements Serializable {
-  public String name = "test"
+    public String name = "test"
 
     Tester(String pName) {
         this.name = pName
-          }
+    }
 
-            def sayHi() {
-                echo "Hello, ${this.name}."
-                  }
+    def sayHi() {
+        echo "Hello, ${this.name}."
+    }
 
-                    def sayHi(String name) {
-                        echo "Hello, ${name}."
-                          }
-                          }
+    def sayHi(String name) {
+        echo "Hello, ${name}."
+    }
+}
