9f71c2140dcee5ea81520bfc945389753c4fda0e
cdh <dh.chandu@gmail.com>
2020-03-11 10:53:08 +0530

    dynamic function call using shared libs class

---
+++ src/jnpr/releng/dynamicFuncCall.groovy
@@ -0,0 +1,36 @@
+package jnpr.releng
+
+class dynamicFunc {
+    def ui(value) {
+        return {
+            stage (name) {
+                stage(name+"checkout") {
+                    echo "start 1"
+                    sleep 1
+                    echo "done 1"
+                }
+                stage("build") {
+                    echo "start 2"
+                    sleep 1
+                    echo "done 2"
+                }
+            }
+        }
+    }
+    def bcm() {
+        return {
+            stage (name) {
+                stage(name+"checkout") {
+                    echo "start 1"
+                    sleep 1
+                    echo "done 1"
+                }
+                stage("build") {
+                    echo "start 2"
+                    sleep 1
+                    echo "done 2"
+                }
+            }
+        }
+    }
+}
