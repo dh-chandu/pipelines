d18bcaf3a1ab8b17ec89f1111d717af7cec809f0
cdh <dh.chandu@gmail.com>
2020-03-11 16:05:24 +0530

    dynamic

--- src/jnpr/releng/Testerer.groovy
+++ src/jnpr/releng/Testerer.groovy
@@ -4,11 +4,12 @@ package jnpr.releng
 public class Testerer implements Serializable {
     public String name = "test"
 
-    Testerer(String pName) {
-        this.name = pName
-    }
+//    Testerer(String pName) {
+//        this.name = pName
+//    }
 
     def sayHi(name) {
-        println "Hello, ===== ${this.name}."
+        //println "Hello, ===== ${this.name}."
+        println "Hello, ===== "
     }
 }
