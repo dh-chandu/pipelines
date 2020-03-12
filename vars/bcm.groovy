77db52b8701e3807ab1e456451a064475f619907
cdh <dh.chandu@gmail.com>
2020-03-12 17:49:21 +0530

    scripts

--- vars/bcm.groovy
+++ vars/bcm.groovy
@@ -1,20 +1,23 @@
 def call(String name = '') {
      return {
         stage (name) {
-            stage(name+" checkout") {
-                sh "echo ${env.WORKSPACE}/../${env.JOB_NAME}"
-                sh " ls -ltr ${env.WORKSPACE}/../${env.JOB_NAME}"
-                echo "$name checkout "
-                echo "$name checkout done"
+            node {
+                stage(name+" checkout") {
+                    sh "echo ${env.WORKSPACE}/../${env.JOB_NAME}"
+                    sh " ls -ltr ${env.WORKSPACE}/../${env.JOB_NAME}"
+                    echo "$name checkout "
+                    echo "$name checkout done"
+                }
+                stage(name+" Build") {
+                    echo "$name build "
+                    echo "$name build done "
+                }
+                stage(name+" test") {
+                    echo "$name Test "
+                    echo "$name Test done "
+                }
             }
-            stage(name+" Build") {
-                echo "$name build "
-                echo "$name build done "
-            }
-            stage(name+" test") {
-                echo "$name Test "
-                echo "$name Test done "
-            }
+
         }
     }
 }
