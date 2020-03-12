a31cd4afcd8abd1288480a2986a8e390d0328da2
cdh <dh.chandu@gmail.com>
2020-03-12 10:07:08 +0530

    dynamic

--- src/paralle_sequence.groovy
+++ src/paralle_sequence.groovy
@@ -5,34 +5,35 @@ pipeline {
     agent none
 
     stages {
-        stage ("Prepare Stages"){
+        stage("Prepare Stages") {
             steps {
                 script {
 //                    for (int i = 1; i < 300; i++) {
 //                        stepsToRun["Step${i}"] = prepareStage("Step${i}")
 //                    }
                     String[] components = ['bcm', 'ui']
-                    for (component in components){
+                    for (component in components) {
 //                    for (int i = 0; i < components.length; i++) {
-                        println '-component---'+i+'-----'+components[i]
+                        println '-component---' + i + '-----' + components[i]
                         def tmp = components[i]
-                        println '====tmp==='+tmp
+                        println '====tmp===' + tmp
                         def tmp_component = tmp
-                        println '====tmp_component ==='+tmp_component
-                        if (tmp == 'bcm'){
+                        println '====tmp_component ===' + tmp_component
+                        if (tmp == 'bcm') {
                             stepsToRun[tmp_component] = bcm(tmp_component)
 
                         }
-                        if (tmp == 'ui'){
+                        if (tmp == 'ui') {
                             stepsToRun[tmp_component] = ui(tmp_component)
                         }
 
 //                    stepsToRun['bcm'] = bcm('bcm')
 //                    stepsToRun['ui'] = ui('ui')
-                    parallel stepsToRun
+                        parallel stepsToRun
+                    }
                 }
             }
         }
     }
-}
 
+}
