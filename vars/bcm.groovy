a95dff8e9f5fd137f35b4d93d254bb9acf73d926
cdh <dh.chandu@gmail.com>
2020-03-12 19:25:36 +0530

    scripts

--- vars/bcm.groovy
+++ vars/bcm.groovy
@@ -4,7 +4,8 @@ def call(String name = '') {
             node {
                 stage(name+" checkout") {
                     sh "echo ${env.WORKSPACE}/../${env.JOB_NAME}"
-                    def files = sh(returnStdout: true, script: 'ls -ltr ${env.WORKSPACE}/').trim()
+                    def cmd = 'ls -ltr '+${env.WORKSPACE}
+                    def files = sh(returnStdout: true, script: cmd).trim()
                     println '----files---'+files
                     echo "$name checkout "
                     echo "$name checkout done"
