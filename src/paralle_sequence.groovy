e25d7443751bab03398e167433f4cbabfececfac
cdh <dh.chandu@gmail.com>
2020-03-12 10:16:41 +0530

    dynamic

--- src/paralle_sequence.groovy
+++ src/paralle_sequence.groovy
@@ -8,14 +8,12 @@ pipeline {
         stage ("Prepare Stages"){
             steps {
                 script {
-//                    for (int i = 1; i < 300; i++) {
-//                        stepsToRun["Step${i}"] = prepareStage("Step${i}")
-//                    }
+
                     String[] components = ['bcm', 'ui']
-//                    for (component in components){
-                    for (int i = 0; i < components.length; i++) {
-                        println '-component---'+i+'-----'+components[i]
-                        def tmp = components[i]
+                    for (component in components){
+ //                   for (int i = 0; i < components.length; i++) {
+//                        def tmp = components[i]
+                        def tmp = component
                         println '====tmp==='+tmp
                         def tmp_component = tmp
                         println '====tmp_component ==='+tmp_component
@@ -26,19 +24,6 @@ pipeline {
                         if (tmp == 'ui'){
                             stepsToRun[tmp_component] = ui(tmp_component)
                         }
-//                        switch (tmp_component){
-//                            case 'bcm':
-//                                println 'inside swich bcm'+tmp_component
-//                                stepsToRun[tmp_component] = bcm(tmp_component)
-//                            case 'ui':
-//                                println 'inside swich ui'+tmp_component
-//                                stepsToRun[tmp_component] = ui(tmp_component)
-//                            default:
-//                                println 'waste'
-//                        }
-                    }
-//                    stepsToRun['bcm'] = bcm('bcm')
-//                    stepsToRun['ui'] = ui('ui')
                     parallel stepsToRun
                 }
             }
